edc8e7114df85f7324ccc50912032fccf0afe029
Nadezhda Telepenina <Telepenina.N.S@yandex.ru>
2025-11-04 22:04:26 +0400

    Bugfix main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,6 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import { createYmaps } from 'vue-yandex-maps'
 
-// Функция для получения API ключа из переменных окружения или глобальной переменной
-const getApiKey = (): string => {
-  // @ts-expect-error: window.VITE_YANDEX_API_KEY инжектируется через HTML
-  return import.meta.env?.YANDEX_API_KEY || window.YANDEX_API_KEY
-}
-
 const app = createApp(App)
 
 // Функция для получения API ключа из переменных окружения или глобальной переменной
